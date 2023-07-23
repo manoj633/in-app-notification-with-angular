3b8f70ebfee9e9ef9f716eed0f90d8122b560308
Manoj <manoj09633@gmail.com>
2023-07-23 21:34:40 +0530

    Added styles and env variables into component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'in-app-notification-with-angular';
+
+  subscriberId = environment.subscriberId;
+  applicationIdentifier = environment.applicationIdentifier;
+
+  styles = {
+    bellButton: {
+      root: {
+        svg: {
+          color: 'white',
+          width: '45px',
+          height: '40px',
+          fill: 'white',
+        },
+      },
+      dot: {
+        rect: {
+          fill: 'rgb(221, 0, 49)',
+          strokeWidth: '0.2',
+          stroke: 'white',
+          width: '3.5px',
+          height: '3.5px',
+        },
+        left: '40%',
+      },
+    },
+    header: {
+      root: {
+        backgroundColor: '',
+        '&:hover': { backgroundColor: '' },
+        '.some_class': { color: '' },
+      },
+    },
+    layout: {
+      root: {
+        backgroundColor: '',
+      },
+    },
+    popover: {
+      arrow: {
+        backgroundColor: '',
+        border: '',
+      },
+    },
+  };
+
+  sessionLoaded = (data: unknown) => {
+    console.log('loaded', { data });
+  };
 }
