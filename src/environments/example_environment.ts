04f3b17bd882e936cebe7054cbbc721c48a93888
Manoj <manoj09633@gmail.com>
2023-07-23 21:33:15 +0530

    Added environment variables

---
+++ src/environments/example_environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  subscriberId: "<REPLACE_WITH_USER_UNIQUE_IDENTIFIER>",
+  applicationIdentifier: "<REPLACE_WITH_APP_ID_FROM_ADMIN_PANEL>"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
